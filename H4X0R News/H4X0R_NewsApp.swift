29a36674b53692e1c858d8b64cd0c607c2e82c39
diasGustaavo <diasgustaavo@icloud.com>
2022-12-26 11:51:40 -0300

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Gustavo Dias on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
